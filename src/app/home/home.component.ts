a66e451b11c3b8df43597e90791c7fd507ce1b22
worldcoolguy <miguelsantiagoupwork925@gmail.com>
2017-05-23 23:10:26 -0400

    refactor code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
 import { Injectable } from '@angular/core';
+import {PackageDataService} from '../common/packageDataService';
 
 @Component({
     selector: 'app-home',
@@ -8,6 +9,18 @@ import { Injectable } from '@angular/core';
 })
 
 export class HomeComponent implements OnInit {
-    constructor() {}
-    ngOnInit() {}
+    URL_NugetPackages = './assets/nugetpackages.json';
+    nugetData:any[]
+
+   constructor(private _packageDataService: PackageDataService) { }
+    ngOnInit() {
+        this._packageDataService
+            .getExtensionsData(this.URL_NugetPackages)
+            .then((data) => {   
+                this.nugetData=data;
+            })
+            .catch((err) => {
+                console.log(err); // dont do this, show the user a nice message
+            });
+        }
 }
