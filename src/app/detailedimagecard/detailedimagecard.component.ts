3c7460ae49bf22b9956fa0a3708c8ff3640c44c2
worldcoolguy <miguelsantiagoupwork925@gmail.com>
2018-04-06 02:33:43 -0400

    refactor chart page

--- src/app/detailedimagecard/detailedimagecard.component.ts
+++ src/app/detailedimagecard/detailedimagecard.component.ts
@@ -13,11 +13,18 @@ export class DetailedImageCardComponent implements OnInit {
     packageData: any;
     methodNames:any[];
     showhideSourceLink:any;
+    config:{};
 
     constructor(
         private _packageDataService: PackageDataService,
         private _route :  ActivatedRoute
-    ) {}
+    ) {
+        this.config={ 
+            lineNumbers: true, 
+            mode: 'python',
+            theme:'mdn-like'
+        };
+    }
     ngOnInit() {
         let id = this._route.snapshot.paramMap.get('id').toLowerCase();      
         let key = this._route.snapshot.paramMap.get('key').toLowerCase();  
@@ -26,7 +33,8 @@ export class DetailedImageCardComponent implements OnInit {
         this._packageDataService
             .getExtensionsData(baseUrl)
             .then((returnData) => {
-                this.packageData=returnData                   
+                this.packageData=returnData.find((d:any)=> {return d.title.toLowerCase()==id} );    
+                this.packageData=this.packageData.images.find((d:any)=>d.key.toLowerCase()==key);
             })
             .catch((err) => {
                 console.log(err); // dont do this, show the user a nice message
