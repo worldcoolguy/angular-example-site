739e4539825e055803e729d630eefe760b5f4bc3
worldcoolguy <miguelsantiagoupwork925@gmail.com>
2018-04-19 23:05:45 -0400

    style change

--- src/app/python/pythonds.component.ts
+++ src/app/python/pythonds.component.ts
@@ -9,6 +9,27 @@ import {PackageDataService} from '../common/packageDataService';
 })
 
 export class PythonDsComponent implements OnInit {
-    constructor() {}
-    ngOnInit() {}
+
+    packageData: any;
+    methodNames:any[];
+    showhideSourceLink:any;
+
+    constructor( private _packageDataService: PackageDataService) {}
+    ngOnInit() {
+
+        let baseUrl=`./assets/pythonds.json`;
+
+        this._packageDataService
+            .getExtensionsData(baseUrl)
+            .then((returnData) => {
+                debugger
+                this.packageData = returnData
+                this.methodNames=returnData.data.map((d:any)=> d.methodName);
+                this.showhideSourceLink=this.packageData.gitLink ? true : false;
+            })
+            .catch((err) => {
+                console.log(err); // dont do this, show the user a nice message
+            });        
+
+    }
 }
